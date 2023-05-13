eb48f61eb5af09b904ca202c3e63910bbe662987
SofiaExpirience <sofia.expirience@gmail.com>
2023-05-12 21:29:55 -0400

    FIX convert the action entity into a enum

--- src/main/java/com/example/spring_security_demo/persistance/entity/Action.java
+++ src/main/java/com/example/spring_security_demo/persistance/entity/Action.java
@@ -1,24 +1,22 @@
 package com.example.spring_security_demo.persistance.entity;
-
-import lombok.Getter;
-import lombok.Setter;
-
-import javax.persistence.*;
-import java.util.List;
-
-@Getter
-@Setter
-@Entity
-@Table(name = "actions")
-public class Action {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name="action_id")
-    private Integer actionId;
-
-    @Column(name="action_name")
-    private String actionName;
-
-    @OneToMany(mappedBy = "action")
-    private List<Permission> permissions;
+/*
+* Defines the types of actions that can be taken from a module
+* Define los tipos de acciones que pueden tomarse desde un modulo
+* */
+public enum Action {
+    // query data
+    // requerir datos
+    GET,
+    // insert data
+    // insertar datos
+    POST,
+    // modiffy data
+    // modificar datos
+    PUT,
+    // delete data
+    // borrar datos
+    DELETE,
+    // no action, just to catch errors
+    // ninguna acción sólo para capturar errores
+    NONE
 }
--- src/main/java/com/example/spring_security_demo/persistance/entity/Permission.java
+++ src/main/java/com/example/spring_security_demo/persistance/entity/Permission.java
@@ -24,8 +24,8 @@ public class Permission {
     private Integer roleId;
     @Column(name="module_id")
     private Integer moduleId;
-    @Column(name="action_id")
-    private Integer actionId;
+    @Column(name="action_name")
+    private String actionName;
     @Column(name="permission_name")
     private String permissionName;
 
@@ -48,8 +48,26 @@ public class Permission {
     @JoinColumn(name = "module_id",insertable = false,updatable = false)
     private Module module;
 
-    @ManyToOne
-    @JoinColumn(name = "action_id",insertable = false,updatable = false)
     private Action action;
 
+    public void setActionId(String actionName) {
+        this.actionName = actionName;
+        switch (actionName){
+            case "GET":
+                action=Action.GET;
+                break;
+            case "POST":
+                action=Action.POST;
+                break;
+            case "PUT":
+                action=Action.PUT;
+                break;
+            case "DELETE":
+                action=Action.DELETE;
+                break;
+            default:
+                action=Action.NONE;
+                break;
+        }
+    }
 }
